6ae1733e45c6c74ac7707938d41f5c09268d39fd
doringber123 <dori@pango.co.il>
2021-08-03 08:04:28 +0300

    ;;

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
 docker-compose -f docker-compose-v3.yml up
