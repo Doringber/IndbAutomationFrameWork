433eb97006be4f0515b2eb01e34e2584531519be
doringber123 <dori@pango.co.il>
2021-08-03 09:57:41 +0300

    'ioo'po

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
-curl -fsSL get.docker.com -o get-docker.sh
-sh get-docker.sh
-curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-usermod -aG docker $YOUR_USER
-systemctl enable docker
+cat IndbAutomationFrameWork/run.sh
+docker-compose -f docker-compose-v3.yml up
